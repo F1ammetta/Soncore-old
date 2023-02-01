0ee20e952f67e30b56d25bd651bce38eac1d080f
Sergio <sdepineresramirez@utb.edu.co>
2023-02-01 10:40:38 -0500

    Now playing bar fixed

--- lib/now_playing.dart
+++ lib/now_playing.dart
@@ -54,69 +54,63 @@ class _NowPlayingState extends State<NowPlaying> {
                           },
                         )
                       : const Center(),
-                  // Center(
-                  //   widthFactor: 1,
-                  //   heightFactor: 1,
-                  //   child: Container(
-                  //     alignment: const Alignment(0.0, 0.2),
-                  //     child: ClipRRect(
-                  //       borderRadius: BorderRadius.circular(5),
-                  //       child: AppBar(
-                  //         toolbarHeight: hasplayed ? kToolbarHeight : 0,
-                  //         leading: hasplayed
-                  //             ? Padding(
-                  //                 padding: const EdgeInsets.all(4.5),
-                  //                 child: ClipRRect(
-                  //                   borderRadius: BorderRadius.circular(9.0),
-                  //                   child: Image.network(
-                  //                     'http://kwak.sytes.net/v0/cover/${nowplaying['id']}',
-                  //                     height: 50,
-                  //                     width: 50,
-                  //                   ),
-                  //                 ),
-                  //               )
-                  //             : const Card(
-                  //                 child: AspectRatio(aspectRatio: 1.0),
-                  //               ),
-                  //         title: hasplayed
-                  //             ? GestureDetector(
-                  //                 onTap: widget.showplaying,
-                  //                 child: TextScroll(
-                  //                   nowplaying['title'] +
-                  //                       '  -  ' +
-                  //                       nowplaying['artist'] +
-                  //                       ' ' * nowplaying['title'].length,
-                  //                   mode: TextScrollMode.endless,
-                  //                   velocity: const Velocity(
-                  //                       pixelsPerSecond: Offset(30, 0)),
-                  //                 ),
-                  //               )
-                  //             : const Text(''),
-                  //         titleTextStyle: const TextStyle(fontSize: 20),
-                  //         actions: [
-                  //           IconButton(
-                  //               onPressed: () {
-                  //                 widget.goprevious();
-                  //               },
-                  //               icon: const Icon(Icons.skip_previous)),
-                  //           IconButton(
-                  //               onPressed: widget.playpause,
-                  //               icon: Icon(widget.icon)),
-                  //           IconButton(
-                  //               onPressed: () {
-                  //                 widget.gonext();
-                  //               },
-                  //               icon: const Icon(Icons.skip_next))
-                  //         ],
-                  //       ),
-                  //     ),
-                  //   ),
-                  // )
-                  Image.network(
-                    'http://kwak.sytes.net/v0/cover/${nowplaying['id']}',
-                    height: 50,
-                    width: 50,
-                  ),
+                  Center(
+                    widthFactor: 1,
+                    heightFactor: 1,
+                    child: SizedBox(
+                      height: hasplayed ? kToolbarHeight : 0,
+                      width: 385,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[900],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(4.5),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(9.0),
+                                  child: Image.network(
+                                    'http://kwak.sytes.net/v0/cover/${nowplaying['id']}',
+                                    height: 50,
+                                    width: 50,
+                                  ),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: widget.showplaying,
+                                child: SizedBox(
+                                  width: 180,
+                                  child: TextScroll(
+                                    nowplaying['title'] +
+                                        '  -  ' +
+                                        nowplaying['artist'] +
+                                        ' ' * nowplaying['title'].length,
+                                    mode: TextScrollMode.endless,
+                                    style: const TextStyle(fontSize: 20),
+                                    velocity: const Velocity(
+                                        pixelsPerSecond: Offset(30, 0)),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    widget.goprevious();
+                                  },
+                                  icon: const Icon(Icons.skip_previous)),
+                              IconButton(
+                                  onPressed: widget.playpause,
+                                  icon: Icon(widget.icon)),
+                              IconButton(
+                                  onPressed: () {
+                                    widget.gonext();
+                                  },
+                                  icon: const Icon(Icons.skip_next))
+                            ],
+                          )),
+                    ),
+                  )
                 ],
               ),
             ),
